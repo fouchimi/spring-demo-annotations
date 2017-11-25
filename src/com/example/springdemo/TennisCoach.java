f5740d0dad6fa0d2a3f585c0f96df684baa77b85
Fouchimi <ousmanefouchimi@gmail.com>
2017-11-24 21:37:37 -0700

    Used @PreConstruct and @Predestroy annotations

--- src/com/example/springdemo/TennisCoach.java
+++ src/com/example/springdemo/TennisCoach.java
@@ -1,12 +1,13 @@
 package com.example.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
@@ -17,6 +18,15 @@ public class TennisCoach implements Coach {
 		System.out.println(">> TennisCoah: inside default constructor");
 	}
 	
+	@PostConstruct
+	public void doMyStartStuff() {
+		System.out.println(">> TennisCoach: inside doMyStartStuff");
+	}
+	
+	@PreDestroy
+	public void doMyCleanUpStuff() {
+		System.out.println(">> TennisCoach: inside doCleanUpStuff");
+	}
 	
 	@Override
 	public String getDailyWorkout() {
